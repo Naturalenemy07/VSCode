c0693e77d1886ef78669935d16ec852a1c860a65
Naturalenemy07 <john.d.caruthers@gmail.com>
2022-11-20 08:51:57 -0500

    enemy class created

--- PSE524/finalProject/mathDefenseScript.js
+++ PSE524/finalProject/mathDefenseScript.js
@@ -11,26 +11,45 @@ c.fillRect(0,0,canvas.width,canvas.height);
 //Load background
 const image = new Image();
 image.onload = () => {
-    animate()
+    animate();
 }
 image.src = 'image/towerDefense.png'
 
-console.log(c);
+//Class for enemies
+class Enemy {
+    //construct enemy with position and dimensions
+    constructor({position = {x: 0, y: 0}}) {
+        this.position = position
+        this.width = 20;
+        this.height = 20;
+    } 
+
+    //draw enemy
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x,this.position.y,this.width, this.height);
+    }
+
+    //update enemy properties, call draw each time
+    update() {
+        this.draw();
+        this.position.x += 1;
+    }
+}
+
+//create enemies
+const enemy1 = new Enemy({position: {x: 20, y: 40}})
+const enemy2 = new Enemy({position: {x: 25, y: 60}})
 
 //will animate the enemy moving along waypoints
-let x = 60;
-let y = 40
 function animate() {
     //recursively call animate()
     requestAnimationFrame(animate);
 
     //draw background
-    c.drawImage(image,0,0)
+    c.drawImage(image,0,0);
 
-    // drawing enemy
-    c.fillStyle='red';
-    c.fillRect(x,y,30,30);
-    x++;
-    y++;
-
+    //draw enemies
+    enemy1.update()
+    enemy2.update()
 }
