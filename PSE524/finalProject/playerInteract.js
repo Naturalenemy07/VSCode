c0bf498e5eab8748cc54f8e029da6c170fdb440e
Naturalenemy07 <john.d.caruthers@gmail.com>
2022-11-27 12:52:10 -0500

    buttons now working

--- PSE524/finalProject/playerInteract.js
+++ PSE524/finalProject/playerInteract.js
@@ -1,16 +1,12 @@
 const numButtons = 3;
 //function to randomly select button intervals
-function buttonRand () {
+function buttonRand() {
     var num = Math.floor(Math.random()*numButtons +1);
     var fulltext = '#button' + String(num);
     return [num, fulltext];
 };
 
-function eqCorrect () {
-    //add money
-    currency += 10;
-    document.querySelector('#currency').innerHTML = currency;
-
+function update() {
     //get a new equation
     problem = new ProblemGen;
     var eq = problem.getEquation();
@@ -38,56 +34,39 @@ function eqCorrect () {
         document.querySelector(newButSel[1]).innerHTML = Math.floor(Math.random()*problem.elementA + Math.random() * 10);
         counter++;
     };
+
+    document.getElementById('button1').addEventListener('click', function() {
+        // console.log('button1 clicked ' + button1.innerHTML);
+        if (button1.innerHTML === eq[1]) {
+            //add money
+            currency += 10;
+            document.querySelector('#currency').innerHTML = currency;
+            update();
+        }
+    }
+    );
+    
+    document.getElementById('button2').addEventListener('click', function() {
+        // console.log('button2 clicked ' + button2.innerHTML);
+        if (button2.innerHTML === eq[1]) {
+            //add money
+            currency += 10;
+            document.querySelector('#currency').innerHTML = currency;
+            update();
+        }
+    }
+    );
+    
+    document.getElementById('button3').addEventListener('click', function() {
+        // console.log('button3 clicked ' + button3.innerHTML);
+        if (button3.innerHTML === eq[1]) {
+            //add money
+            currency += 10;
+            document.querySelector('#currency').innerHTML = currency;
+            update();
+        }
+    }
+    );
 }
 
-// Generates equation and answer, puts equation on screen
-problem = new ProblemGen;
-var eq = problem.getEquation();
-var equationDisp = String(problem.elementA) + " " + eq[0] + " " + String(problem.elementB) + " =";
-document.querySelector('#equation').innerHTML = equationDisp;
-
-// Randomly selects button to put correct answer in
-correctButton = buttonRand();
-var butsel = correctButton[0];
-var butSelTxt = correctButton[1];
-document.querySelector(butSelTxt).innerHTML = eq[1];
-
-//fills in the rest of the buttons
-var counter = 1;
-const buttonsUsed = [];
-buttonsUsed.push(butsel);
-
-// Fills in the rest of the buttons with pseudo random numbers
-while (counter < numButtons) {
-    var newButSel = buttonRand();
-    while (buttonsUsed.includes(newButSel[0])) {
-        newButSel = buttonRand();
-    }
-    buttonsUsed.push(newButSel[0]);
-    document.querySelector(newButSel[1]).innerHTML = Math.floor(Math.random()*problem.elementA + Math.random() * 10);
-    counter++;
-};
-
-document.getElementById('button1').addEventListener('click', function() {
-    // console.log('button1 clicked ' + button1.innerHTML);
-    if (button1.innerHTML === eq[1]) {
-        eqCorrect();
-    }
-}
-);
-
-document.getElementById('button2').addEventListener('click', function() {
-    // console.log('button2 clicked ' + button2.innerHTML);
-    if (button2.innerHTML === eq[1]) {
-        eqCorrect();
-    }
-}
-);
-
-document.getElementById('button3').addEventListener('click', function() {
-    // console.log('button3 clicked ' + button3.innerHTML);
-    if (button3.innerHTML === eq[1]) {
-        eqCorrect();
-    }
-}
-);
+update();
