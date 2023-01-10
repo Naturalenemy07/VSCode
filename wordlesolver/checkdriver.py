b76efa7d9f995bf9e90411f549243f75c9b1d04c
Naturalenemy07 <john.d.caruthers@gmail.com>
2023-01-10 06:24:03 -0500

    implementing auto driver check, in progress

---
+++ wordlesolver/checkdriver.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from webdriver_auto_update import check_driver
+
+check_driver('C:\Users\johnd\Documents\School\Graduate School\Computer Science\VSCode\chromedriver_win32\chromedriver.exe')
